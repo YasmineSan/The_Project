477303b432ef322b0d15c0e31b30291d58706591
yaz <yasmine-sanchez@proton.me>
2024-06-05 14:52:13 +0200

    add files .env

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,11 @@
 const express = require('express'); 
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3000; // use the variable envirronment or a port 3000
 
 
 //pour ouvrir le server npm start 
-app.listen(8000, function() {
-    console.log('Server is running now at port 8000');
+app.listen(PORT, function() {
+    console.log(`Server is running now at ${PORT}`);
 })
