c186358f11e9c11bface671300b7af30016bc8ad
yaz <yasmine-sanchez@proton.me>
2024-06-05 14:40:35 +0200

    Add derectory client for the frontend

---
+++ Server/server.js
@@ -0,0 +1,9 @@
+const express = require('express'); 
+
+const app = express();
+
+
+//pour ouvrir le server npm start 
+app.listen(8000, function() {
+    console.log('Server is running now at port 8000');
+})
