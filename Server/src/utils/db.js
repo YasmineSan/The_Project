1ec5d633c427b84cb3dc58655df7cfccc16630bd
YasmineSan <yasmine-sanchez@proton.me>
2024-06-07 15:45:43 +0200

    fix of DB + connection

--- Server/src/utils/db.js
+++ Server/src/utils/db.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
-dotenv.config();
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    //port: 3306
 });
 
 db.connect((err) => {
