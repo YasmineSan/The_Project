ab1ad167605fa50d09e90625e32a6604907425b9
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-06-11 11:22:49 +0200

    RegisterModif

--- Server/src/controllers/userController.js
+++ Server/src/controllers/userController.js
@@ -14,15 +14,74 @@ exports.getAllUsers = async (req, res) => {
 
 exports.registerUser = async (req, res) => {
     try {
-        const { username, password, email } = req.body;
+        const {
+            username,
+            biography,
+            first_name,
+            last_name,
+            street,
+            street_number,
+            apartment,
+            postal_code,
+            city,
+            password,
+            email,
+            paypal_address,
+            profile_image
+        } = req.body;
 
         const pool = await poolPromise;
         const hashedPassword = await bcrypt.hash(password, 10);
+        const registration_date = new Date();
+
         await pool.request()
             .input('username', username)
+            .input('biography', biography)
+            .input('first_name', first_name)
+            .input('last_name', last_name)
+            .input('street', street)
+            .input('street_number', street_number)
+            .input('apartment', apartment)
+            .input('postal_code', postal_code)
+            .input('city', city)
             .input('hashed_password', hashedPassword)
             .input('email', email)
-            .query('INSERT INTO Users (username, hashed_password, email) VALUES (@username, @hashed_password, @email)');
+            .input('registration_date', registration_date)
+            .input('paypal_address', paypal_address)
+            .input('profile_image', profile_image)
+            .query(`
+                INSERT INTO Users (
+                    username,
+                    biography,
+                    first_name,
+                    last_name,
+                    street,
+                    street_number,
+                    apartment,
+                    postal_code,
+                    city,
+                    hashed_password,
+                    email,
+                    registration_date,
+                    paypal_address,
+                    profile_image
+                ) VALUES (
+                    @username,
+                    @biography,
+                    @first_name,
+                    @last_name,
+                    @street,
+                    @street_number,
+                    @apartment,
+                    @postal_code,
+                    @city,
+                    @hashed_password,
+                    @email,
+                    @registration_date,
+                    @paypal_address,
+                    @profile_image
+                )
+            `);
 
         res.status(201).send({ message: 'User registered successfully' });
     } catch (err) {
