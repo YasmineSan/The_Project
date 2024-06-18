dc6e3c91968d9079269a5c57e1eaa7ef7856f2e6
Lameuhton <aurelianeg2000@gmail.com>
2024-06-18 12:03:00 +0200

    fixed footer

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,19 +4,17 @@ import { Button } from './Button'
 
 const Header = () => {
   return (
-    <div>
-      <nav className='fixed mx-auto border-b border-[#F2F2F2] top-0 left-0 right-0 z-10 bg-white bg-opacity-100'>
-        <div className='flex container lg:py-5 lg:px-10 flex-wrap items-center justify-between mx-auto px-4 py-2'>
-          <NavLink to="/"><img src="craftify.png" alt="logo Craftify" className='w-10'/></NavLink>
+    <header>
+      <nav className='fixed mx-auto border-b-2 border-gold top-0 left-0 right-0 bg-white bg-opacity-100'>
+        <div className='flex container px-4 lg:px-10 flex-wrap items-center justify-between mx-auto '>
+          <NavLink to="/"><img src="craftify.png" alt="logo Craftify" className='w-20'/></NavLink>
           <Button
             content={"S'identifier"}
             path={'/login'}
-            style={"text-white bg-golden hover:bg-slate-800 rounded-full px-5 py-2"}/>
+            style="text-white bg-golden hover:bg-slate-800 rounded-full px-5 py-2"/>
         </div>
       </nav>
-      
-
-    </div>
+    </header>
   )
 }
 
