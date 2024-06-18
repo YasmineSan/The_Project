7f1cd00199cae6e56a9f8f3a61035d343e8ff8ac
Lameuhton <aurelianeg2000@gmail.com>
2024-06-18 17:06:24 +0200

    modified banner carousel

--- client/src/components/home/CarouselBanner.jsx
+++ client/src/components/home/CarouselBanner.jsx
@@ -2,25 +2,39 @@ import React from 'react'
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-
-
-
 const CarouselBanner = () => {
 
   return (
-    <Carousel autoPlay infiniteLoop dynamicHeight showThumbs={false} showStatus={false} swipeable showArrows={false}>
-        <img
-          className="w-100"
-          src="https://picsum.photos/200/100"
-          alt="First slide"
-        />
-        <img
-          className="w-100"
-          src="https://picsum.photos/200/100"
-          alt="Second slide"
-        />
+    <Carousel autoPlay infiniteLoop dynamicHeight showThumbs={false} showStatus={false} swipeable showArrows={false} className="rounded-lg overflow-hidden">
+        <div className='relative'>
+            <img
+            className="w-100"
+            src="https://picsum.photos/200/100"
+            alt="First slide"
+          />
+        <p className='absolute left-5 lg:left-20 md:top-20 sm:to-10 top-5 text-1xl sm:text-2xl lg:text-3xl md:text-3xl xl:text-4xl font-medium text-white'>Des créations uniques, façonnées avec passion</p>
+        </div>
+        
+        <div className='relative'>
+            <img
+            className="w-100"
+            src="https://picsum.photos/200/100"
+            alt="First slide"
+          />
+        <p className='absolute left-5 lg:left-20 md:top-20 sm:to-10 top-5 text-1xl sm:text-2xl lg:text-3xl md:text-3xl xl:text-4xl font-medium text-white'>Des créations uniques, façonnées avec passion</p>
+        </div>
+
+        <div className='relative'>
+            <img
+            className="w-100"
+            src="https://picsum.photos/200/100"
+            alt="First slide"
+          />
+        <p className='absolute left-5 lg:left-20 md:top-20 sm:to-10 top-5 text-1xl sm:text-2xl lg:text-3xl xl:text-4xl md:text-3xl font-medium text-white'>Des créations uniques, façonnées avec passion</p>
+        </div>
     </Carousel>
     );
 }
 
+export default CarouselBanner;
 
