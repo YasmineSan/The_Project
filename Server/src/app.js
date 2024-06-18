3ce789cacf830770a0594c95da8c02908f89e037
YasmineSan <yasmine-sanchez@proton.me>
2024-06-18 15:01:59 +0200

    Feat securité + add doc api en md

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const multer = require('multer');
 
-// Middleware
+// Middleware pour sécuriser les en-têtes HTTP
+app.use(helmet());
+
+// Middleware pour limiter le nombre de requêtes
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
+});
+
+app.use(limiter);
+
+// Middleware pour parser les requêtes JSON et URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
