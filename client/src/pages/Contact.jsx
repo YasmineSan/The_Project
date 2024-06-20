7f59add7e4669034394c00706633798ed6128391
ovo-thom <thonnard.thomas@gmail.com>
2024-06-20 12:23:22 +0200

    page contact fini

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Contact = () => {
+  // États pour gérer les champs du formulaire et le message de confirmation
+  const [email, setEmail] = useState('');
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [message, setMessage] = useState('');
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Simuler l'envoi du message
+    setConfirmationMessage('Votre message a été envoyé avec succès !');
+
+    // Réinitialiser les champs du formulaire
+    setEmail('');
+    setNom('');
+    setPrenom('');
+    setMessage('');
+  };
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center pt-28">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-4 mt-2">Contactez-nous</h1>
+        <h1 className="text-3xl font-semibold text-center mb-4 mt-2">Contactez-nous</h1>
         <p className="text-center text-gray-700 mb-8">
           Vous avez une question ou une réclamation? N’hésitez pas à prendre contact avec nous via le formulaire ci-dessous. Nous vous répondrons dans les plus brefs délais.
         </p>
         <div className="flex flex-wrap justify-center">
           <div className="w-full md:w-1/2 p-4">
-            <form className="bg-white p-6 rounded-lg shadow-lg">
+            <form className="bg-white p-6 rounded-lg shadow-lg" onSubmit={handleSubmit}>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="email">
                   Email
                 </label>
                 <input
@@ -20,10 +40,13 @@ const Contact = () => {
                   id="email"
                   type="email"
                   placeholder="Votre email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nom">
+                <label className="block text-gray-700 text-sm mb-2 font-semibold" htmlFor="nom">
                   Nom
                 </label>
                 <input
@@ -31,10 +54,13 @@ const Contact = () => {
                   id="nom"
                   type="text"
                   placeholder="Votre nom"
+                  value={nom}
+                  onChange={(e) => setNom(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="prenom">
+                <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="prenom">
                   Prénom
                 </label>
                 <input
@@ -42,10 +68,13 @@ const Contact = () => {
                   id="prenom"
                   type="text"
                   placeholder="Votre prénom"
+                  value={prenom}
+                  onChange={(e) => setPrenom(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+                <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="message">
                   Votre message
                 </label>
                 <textarea
@@ -53,17 +82,25 @@ const Contact = () => {
                   id="message"
                   placeholder="Votre message"
                   rows="4"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
                 ></textarea>
               </div>
               <div className="flex items-center justify-between">
                 <button
-                  className="w-full bg-gold text-white py-2 rounded hover:bg-darkGold hover:bg-inherit hover:text-gold hover:border hover:border-gold"
-                  type="button"
+                  className="w-full bg-gold text-white py-2 rounded hover:bg-darkGold hover:bg-inherit hover:text-gold border border-gold hover:border hover:border-gold"
+                  type="submit"
                 >
                   Envoyer
                 </button>
               </div>
             </form>
+            {confirmationMessage && (
+              <div className="mt-4 p-4 bg-green-200 text-green-800 rounded">
+                {confirmationMessage}
+              </div>
+            )}
           </div>
           <div className="w-full md:w-1/2 p-4">
             <div className="bg-white p-6 rounded-lg shadow-lg h-full">
@@ -85,4 +122,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact;
