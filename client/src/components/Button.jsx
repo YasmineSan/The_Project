e1bc9846e30148610ecab68a282a8e210201e058
Lameuhton <aurelianeg2000@gmail.com>
2024-06-21 10:14:32 +0200

    details

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom'
 
 export const Button = ({content, path, style}) => {
   return (
-    <button>
-        <NavLink to={path} className={style}>
+    <button className={style}>
+        <NavLink to={path}>
             {content}
         </NavLink>
     </button>
