51c962cdeb93a8e952f86096f832a9e88dcf4586
Lameuhton <aurelianeg2000@gmail.com>
2024-06-27 20:05:13 +0200

    added logic to delete article from cart (to test)

--- client/src/pages/UserCart.jsx
+++ client/src/pages/UserCart.jsx
@@ -88,31 +88,32 @@ export const UserCart = () => {
         throw new Error('Failed to delete item');
       }
 
-      setCart(await response.json());
+      const updatedCart = cart.filter(item => item.id !== id);
+      setCart(updatedCart);
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
   const handleQuantityChange = async (id, quantity) => {
-    try {
-      const response = await fetch(`https://4.233.138.141:3001/api/cart/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('authToken')}`
-        },
-        body: JSON.stringify({ quantity })
-      });
+    // try {
+    //   const response = await fetch(`https://4.233.138.141:3001/api/cart/${id}`, {
+    //     method: 'PUT',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+    //     },
+    //     body: JSON.stringify({ quantity })
+    //   });
 
-      if (!response.ok) {
-        throw new Error('Failed to update quantity');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('Failed to update quantity');
+    //   }
 
-      setCart(cart.map(item => item.id === id ? { ...item, quantity } : item));
-    } catch (error) {
-      console.error('Error:', error);
-    }
+    //   setCart(cart.map(item => item.id === id ? { ...item, quantity } : item));
+    // } catch (error) {
+    //   console.error('Error:', error);
+    // }
   };
 
   const subtotal = cart.reduce((acc, item) => acc + item.article_price * item.quantity, 0);
@@ -137,12 +138,12 @@ export const UserCart = () => {
               <div key={item.article_id} className="border rounded-lg p-4 mb-4 relative transition-transform duration-300 hover:scale-105">
                 <div className="flex flex-col md:flex-row items-start mb-4">
                   <NavLink to={`/articles/:${item.article_id}`} className="mr-0 md:mr-8 mb-4 md:mb-0">
-                    <img src={item.article_image} alt={item.article_title} className="w-32 h-32 object-cover" />
+                    <img src={item.article_photo} alt={item.title} className="w-32 h-32 object-cover" />
                   </NavLink>
                   <div className="flex-1">
                     <div className="text-xl mb-2 font-medium">{item.article_price}€</div>
                     <NavLink to={`/articles/:${item.article_id}`}>
-                      <h2 className="text-lg font-semibold">{item.article_title}</h2>
+                      <h2 className="text-lg font-semibold">{item.title}</h2>
                     </NavLink>
                     <p className="text-gray-600">{item.article_description.length > 100 ? item.article_description.slice(0, 100) + '...' : item.article_description}</p>
                     <div className="mt-2">
@@ -157,7 +158,7 @@ export const UserCart = () => {
                       <label className="mr-2">Quantité:</label>
                       <select
                         value={item.quantity}
-                        onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
+                        onChange={(e) => handleQuantityChange(item.article_id, parseInt(e.target.value))}
                         className="border rounded px-2 py-1 transition duration-300 hover:bg-gray-200"
                       >
                         {[...Array(10).keys()].map(x => (
@@ -167,7 +168,7 @@ export const UserCart = () => {
                     </div>
                   </div>
                 </div>
-                <button onClick={() => handleRemoveItem(item.id)} className="absolute top-4 right-4 mt-2 mr-2 lg:ml-4 transition-transform duration-300 hover:scale-125">
+                <button onClick={() => handleRemoveItem(item.article_id)} className="absolute top-4 right-4 mt-2 mr-2 lg:ml-4 transition-transform duration-300 hover:scale-125">
                   <FiX className="text-red-600 w-6 h-6" />
                 </button>
               </div>
@@ -213,6 +214,7 @@ export const UserCart = () => {
 };
 
 export default UserCart;
+
 
 
 
