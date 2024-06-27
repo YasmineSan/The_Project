9fd08780bdf5018577e37201a45a186a4c1ddc08
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-06-27 11:00:55 +0200

    removebruteforceprotection

--- Server/src/app.js
+++ Server/src/app.js
@@ -15,10 +15,10 @@ app.use(cors({
 }));
 
 // Middleware pour limiter le nombre de requêtes
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100 // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
+// });
 
 app.use(limiter);
 
