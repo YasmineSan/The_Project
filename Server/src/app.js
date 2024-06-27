6191c1fa4970db374257007f85a34b9e079d91db
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-06-27 11:02:29 +0200

    removebruteforceprotectionbug

--- Server/src/app.js
+++ Server/src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 //   max: 100 // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
 // });
 
-app.use(limiter);
+// app.use(limiter);
 
 // Middleware pour parser les requêtes JSON et URL-encoded
 app.use(express.json({ limit: '50mb' }));
