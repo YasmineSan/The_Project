2bd952468e8e9953219f1fddcdc04e2381a8d744
Lameuhton <aurelianeg2000@gmail.com>
2024-07-02 15:51:56 +0200

    details

--- client/src/pages/AllSales.jsx
+++ client/src/pages/AllSales.jsx
@@ -32,15 +32,15 @@ const AllSales = () => {
     <div className="container mx-auto px-4 pt-28 pb-8 bg-slate-100">
       <h2 className="text-2xl font-semibold mb-6">Mes ventes</h2>
       {sales.map(sale => (
-        <div key={sale.id} className="bg-white p-4 mb-6 rounded-lg shadow-lg">
+        <div key={sale.id} className="bg-white p-4 mb-6 mx-8 sm:mx-32 lg:mx-0 rounded-lg shadow-lg">
           <div className="flex flex-col lg:flex-row items-center lg:items-start">
             <img src={sale.imageUrl} alt={`Image de ${sale.title}`} className="w-full lg:w-64 h-auto object-cover rounded-lg mb-4 lg:mb-0" />
-            <div className="text-center lg:text-left lg:ml-8 flex-1">
+            <div className="text-center lg:text-left lg:ml-8 flex-1 mb-2">
               <h3 className="text-lg font-semibold">{sale.title}</h3>
-              <p className="text-lg font-bold text-gold">{sale.price}</p>
+              <p className="text-lg font-semibold text-gold">{sale.price}</p>
               <p className="text-gray-600 mb-2">{sale.description}</p>
             </div>
-            <div className="lg:w-1/3 ml-8 lg:ml-12">
+            <div className="lg:w-1/3 ml-8 lg:ml-12 self-start">
               <p className="text-gray-700 mb-2"><span className="font-semibold">Acheté par :</span> {sale.Username}</p>
               <p className="text-gray-700 mb-2"><span className="font-semibold">Nom :</span> {sale.LastName}</p>
               <p className="text-gray-700 mb-2"><span className="font-semibold">Prénom :</span> {sale.FirstName}</p>
