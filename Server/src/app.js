084b5541be61a3d0c79fb9f56032063deb1dd00f
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-07-09 14:16:16 +0200

    fixcannetget62

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,9 +4,6 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const app = express();
-const upload = require('./middleware/uploadImage'); // Importer le middleware d'upload
-const orderRoutes = require('./routes/orderRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
 
 // Middleware pour sécuriser les en-têtes HTTP
 app.use(helmet());
@@ -16,7 +13,7 @@ app.use(cors({
   origin: 'http://localhost:5173' // Remplace par l'origine de ton frontend
 }));
 
-// Middleware pour limiter le nombre de requêtes
+// // Middleware pour limiter le nombre de requêtes
 // const limiter = rateLimit({
 //   windowMs: 15 * 60 * 1000, // 15 minutes
 //   max: 100 // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
@@ -36,44 +33,32 @@ app.use(express.json({
   }
 }));
 
-// Importer et utiliser les routes webhook
-const webhookRoutes = require('./routes/webhookRoutes');
-app.use('/api', webhookRoutes);
-
-// Routes
+// Importer et utiliser les routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/articles', require('./routes/articleRoutes'));
 app.use('/api/categories', require('./routes/categoryRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/favorites', require('./routes/favoriteRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
-app.use('/orders', orderRoutes);
-
-// Utiliser les routes des commandes
-app.use('/api/orders', orderRoutes);
-
-// Utiliser les routes des paiements
-app.use('/api/payments', paymentRoutes);
-
-// Utiliser les routes des évaluations
-const evaluationRoutes = require('./routes/evaluationRoutes');
-app.use('/api/evaluations', evaluationRoutes);
+app.use('/api/payments', require('./routes/paymentRoutes'));
+app.use('/api/evaluations', require('./routes/evaluationRoutes'));
+app.use('/api', require('./routes/webhookRoutes'));
 
 // Serve HTML files
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(dirname, 'views', 'login.html'));
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.join(dirname, 'views', 'register.html'));
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
 });
 
 app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(dirname, 'views', 'dashboard.html'));
+  res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
 });
 
 app.get('/profile', (req, res) => {
-  res.sendFile(path.join(dirname, 'views', 'profile.html'));
+  res.sendFile(path.join(__dirname, 'views', 'profile.html'));
 });
 
 app.get('/', (req, res) => {
