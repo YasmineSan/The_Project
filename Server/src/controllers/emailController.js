6fae21a99fbd8367b03b89aa71d97a55e1a8afc6
YasmineSan <yasmine-sanchez@proton.me>
2024-07-10 15:42:04 +0200

    chore: Update package.json and package-lock.json to include node-mailjet dependency

--- Server/src/controllers/emailController.js
+++ Server/src/controllers/emailController.js
@@ -1,35 +1,37 @@
-const fetch = require('node-fetch');
+const Mailjet = require('node-mailjet');
+
+const mailjet = Mailjet.apiConnect(
+  process.env.MAILJET_API_KEY,
+  process.env.MAILJET_API_SECRET
+);
 
 exports.sendConfirmationEmail = async (req, res) => {
   const { email, username } = req.body;
 
-  try {
-    const response = await fetch('http://4.233.138.141:3001/api/sendConfirmationEmail', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
+  const request = mailjet.post("send", { 'version': 'v3.1' }).request({
+    "Messages": [{
+      "From": {
+        "Email": "craftifywebmail@gmail.com",
+        "Name": "No-reply Craftify"
       },
-      body: JSON.stringify({ email, username })
+      "To": [{
+        "Email": email,
+        "Name": username
+      }],
+      "Subject": "Confirmation d'inscription",
+      "TextPart": `Bonjour ${username}, merci de vous être inscrit!`,
+      "HTMLPart": `<h3>Bonjour ${username},</h3><p>Merci de vous être inscrit!</p>`,
+      "CustomID": "AppGettingStartedTest"
+    }]
+  });
+
+  request
+    .then((result) => {
+      console.log(result.body);
+      res.status(200).send({ message: 'Email de confirmation envoyé avec succès.' });
+    })
+    .catch((err) => {
+      console.error(err.statusCode);
+      res.status(500).send({ message: 'Erreur lors de l\'envoi de l\'email de confirmation.', error: err.message });
     });
-
-    const text = await response.text(); // Obtenez le texte brut de la réponse
-    console.log('Response text:', text); // Loggez la réponse brute
-
-    try {
-      const data = JSON.parse(text); // Essayez de parser le texte en JSON
-      if (response.ok) {
-        console.log('Email de confirmation envoyé avec succès.');
-        res.status(200).send({ message: 'Email de confirmation envoyé avec succès.' });
-      } else {
-        console.error('Erreur lors de l\'envoi de l\'email de confirmation:', data.message || 'Une erreur est survenue.');
-        res.status(500).send({ message: 'Erreur lors de l\'envoi de l\'email de confirmation.' });
-      }
-    } catch (error) {
-      console.error('Erreur lors du parsing JSON:', error); // Loggez l'erreur de parsing JSON
-      res.status(500).send({ message: 'Erreur lors du parsing JSON.', error: error.message });
-    }
-  } catch (error) {
-    console.error('Erreur réseau lors de l\'envoi de l\'email de confirmation:', error);
-    res.status(500).send({ message: 'Erreur réseau lors de l\'envoi de l\'email de confirmation.', error: error.message });
-  }
 };
