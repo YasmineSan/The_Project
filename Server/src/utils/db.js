3fe39b33b8c274afb33f04a8942db2cae132d545
YasmineSan <yasmine-sanchez@proton.me>
2024-07-11 20:16:47 +0200

    chore: Remove unnecessary console logs in db.js

--- Server/src/utils/db.js
+++ Server/src/utils/db.js
@@ -3,12 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Ajout de logs pour vérifier les variables d'environnement
-console.log('DB_SERVER:', process.env.DB_SERVER);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_PORT:', process.env.DB_PORT);
 
 const config = {
     host: process.env.DB_SERVER,
