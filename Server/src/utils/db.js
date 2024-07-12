fa21f17eb18f67694dfb2cf2727cc051c344a759
YasmineSan <yasmine-sanchez@proton.me>
2024-07-12 11:05:58 +0200

    chore: Update MySQL connection in db.js FIX DB.js

--- Server/src/utils/db.js
+++ Server/src/utils/db.js
@@ -1,34 +1,20 @@
-const sql = require('mssql');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
-  port: parseInt(process.env.DB_PORT, 10),
-  options: {
-    encrypt: true, // True si SSL doit être utilisé
-    trustServerCertificate: false, // Pour les environnements de production, utiliser true pour les certificats auto-signés
-    cryptoCredentialsDetails: {
-      minVersion: 'TLSv1.2' // Utilisez TLS v1.2 ou supérieur
-    }
-  }
+    host: process.env.DB_SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT, 10)
 };
 
-const poolPromise = new sql.ConnectionPool(config)
-  .connect()
-  .then(pool => {
-    console.log('Connected to MSSQL');
-    return pool;
-  })
-  .catch(err => {
-    console.error('Database Connection Failed! Bad Config: ', err);
-    process.exit(1); // Arrêtez le serveur si la connexion à la base de données échoue
-  });
+const pool = mysql.createPool(config);
 
 module.exports = {
-  sql, poolPromise
+    pool
 };
+
+
