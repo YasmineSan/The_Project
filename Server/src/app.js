f7945bd6cde035387d73f8dfd0e19eec58e61534
YasmineSan <yasmine-sanchez@proton.me>
2024-07-17 21:47:37 +0200

    chore: Update origin URL in app.js for deployed frontend

--- Server/src/app.js
+++ Server/src/app.js
@@ -11,7 +11,7 @@ app.use(helmet());
 // Middleware pour gérer les CORS
 app.use(
   cors({
-    origin: "https://ecommerce-craftify.netlify.app", // Remplace par l'origine de ton frontend déployé
+    origin: "https://main--ecommerce-craftify.netlify.app", // Remplace par l'origine de ton frontend déployé
   }),
 );
 
