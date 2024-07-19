484124bf486fc7ade31ff818efd3df06d245ee16
YasmineSan <yasmine-sanchez@proton.me>
2024-07-19 22:21:23 +0200

    viteconfig update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,22 +2,28 @@ import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
 
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  const isDevelopment = command === 'serve';
+
+  return {
     server: {
-        port: 5173,
-        strictPort: true,
-        host: '0.0.0.0', // Allow access from any network
-        proxy: {
-            '/api': {
-                target: 'https://craftify-ekc4.onrender.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        },
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'selfsigned.key')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'selfsigned.crt')),
+      port: 5173,
+      strictPort: true,
+      host: '0.0.0.0', // Allow access from any network
+      proxy: {
+        '/api': {
+          target: 'https://craftify-ekc4.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
+      },
+      ...(isDevelopment && {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'selfsigned.key')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'selfsigned.crt')),
+        }
+      })
     },
     // Other possible configurations
+  };
 });
