f383fac3aec24a5cc13c45cde28762f719022fac
YasmineSan <yasmine-sanchez@proton.me>
2024-07-19 23:04:54 +0200

    https remove

--- Server/start-https.js
+++ Server/start-https.js
@@ -1,18 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const http = require('http');
 const app = require('./src/app'); // Ensure correct path to app.js
 
-// HTTPS options
-const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'selfsigned.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'selfsigned.crt')),
-};
-
-// Start the HTTPS server
 const PORT = process.env.PORT || 3001;
 const HOST = '0.0.0.0'; // Ensure the server listens on all network interfaces
 
-https.createServer(httpsOptions, app).listen(PORT, HOST, () => {
-  console.log(`HTTPS Server is running on https://${HOST}:${PORT}`);
+http.createServer(app).listen(PORT, HOST, () => {
+  console.log(`HTTP Server is running on http://${HOST}:${PORT}`);
 });
