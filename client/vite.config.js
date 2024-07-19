4bb0c3154c2d2cfa16b49b29942f6cfdddb3663e
YasmineSan <yasmine-sanchez@proton.me>
2024-07-19 21:39:54 +0200

    vite host udpate

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
     server: {
-        port: 5173, // 
+        port: 5173,
         strictPort: true, // Vite will fail if the port is already in use
         host: true, // Server will be accessible on your local network
     },
